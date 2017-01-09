e7b68ac9d8f353c777ce531c363cebf5d8f23564
borntoknow <borntoknow82@gmail.com>
2017-01-09 16:50:42 +0500

    пробую beautifulsoup

--- scraper/urls.py
+++ scraper/urls.py
@@ -17,8 +17,9 @@ from django.conf.urls import url
 
 
 from scraper.views import scrap
-
-
+from scraper.views import hours_ahead
 urlpatterns = [
-    url(r'^$', scrap, name='scrap')
+    url(r'^$', scrap, name='scrap'),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead)
+
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -1,7 +1,25 @@
+import datetime
+from urllib.request import urlopen
 
-
+from bs4 import BeautifulSoup
+from django.http import Http404
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def scrap(request):
-    return render(request, 'base.html')
+    html = urlopen("https://www.avito.ru/perm/avtomobili/vaz_lada?user=1")
+    bsObj = BeautifulSoup(html)
+    model_list = bsObj.find("select", {"id": "flt_param_280"}).find_all("option")
+
+    return render(request, 'base.html', {'model_list': model_list})
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
